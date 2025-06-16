1b7ea2ae3bb3883933eb8c1e5cd0cec18104b6b9
plan28-06 <planar2006@gmail.com>
2025-06-16 12:59:44 +0530

    Styled the home page with dummy data.

--- server.js
+++ server.js
@@ -7,8 +7,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+const mangas = [
+    {
+        id: 1,
+        img: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSvAzMxVb7pQUlHGjtC8do83vd-UClldQQivBaI3Dq1ny95Ewf4",
+        title: "Attack On Titan",
+        author: "Hajime Isayama",
+    },
+    {
+        id: 2,
+        img: "	https://meo.comick.pictures/0GRgd-m.jpg",
+        title: "Solo Leveling",
+        author: "Chu Gong",
+    },
+];
+
 app.get("/", (req, res, next) => {
-    res.render("home");
+    res.render("home", { mangas: mangas });
 });
 
 app.listen(process.env.PORT, () => {
